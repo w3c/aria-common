976bd79ab81699894d5c074aa2c28b7bdb6234c4
Joanmarie Diggs <jdiggs@igalia.com>
2018-02-05 13:46:29 -0500

    Rely more upon Specref
    The following items are in Specref, but we have slightly different names
    for them locally:
    
    * Our "ACCNAME-AAM": https://www.specref.org/?q=ACCNAME-AAM-1.1
    * Our "ARIA-PRACTICES": https://www.specref.org/?q=WAI-ARIA-PRACTICES-1.1
    * Our "CORE-AAM": https://www.specref.org/?q=CORE-AAM-1.1
    * Our "DPUB-ARIA": https://www.specref.org/?q=DPUB-ARIA-1.0
    * Our "GRAPHICS-AAM": https://www.specref.org/?q=GRAPHICS-AAM-1.0
    * Our "GRAPHICS-ARIA": https://www.specref.org/?q=GRAPHICS-ARIA-1.0
    * Our "HTML-AAM": https://www.specref.org/?q=HTML-AAM-1.0
    * Our "SVG-AAM": https://www.specref.org/?q=SVG-AAM-1.0
    * Our "SVG1": https://www.specref.org/?q=SVG
    
    By replacing local entries with aliasOf references to Specref entries,
    we don't have to change any of our specs, and can still get the benefit
    of letting Specref do the maintenance work.
    
    Ultimately, we should see which of these references are still being used
    in any of our specs, update those specs to use versioned references, and
    then we can remove these entries entirely.

--- biblio.js
+++ biblio.js
@@ -1,104 +1,22 @@
 var biblio = {
 
 	"ACCNAME-AAM": {
-		"title": "Accessible Name and Description: Computation and API Mappings 1.1",
-		"href": "http://www.w3.org/TR/accname-aam-1.1/",
-		"status": "WD",
-		"publisher": "W3C",
-		"authors":  [
-			"Joseph Scheuhammer",
-			"Michael Cooper",
-			"Andi Snow-Weaver",
-			"Aaron Leventhal"
-		],
-		"etAl": true,
-		"deliveredBy":  [
-			"http://www.w3.org/WAI/ARIA/"
-		]
+		"aliasOf": "ACCNAME-AAM-1.1",
 	},
 	"ARIA-PRACTICES": {
-		"title": "WAI-ARIA Authoring Practicess 1.1",
-		"href": "http://www.w3.org/TR/wai-aria-practices-1.1/",
-		"status": "WD",
-		"publisher": "W3C",
-		"authors": [
-			"Matt King",
-			"James Nurthen",
-			"Michael Cooper",
-			"Michiel Bijl",
-			"Joseph Scheuhammer",
-			"Lisa Pappas",
-			"Richard Schwerdtfeger"
-		],
-		"etAl": true,
-		"deliveredBy": [
-			"http://www.w3.org/WAI/ARIA/"
-		]
+		"aliasOf": "WAI-ARIA-PRACTICES-1.1",
 	},
 	"CORE-AAM": {
-		"title": "Core Accessibility API Mappings 1.1",
-		"href": "http://www.w3.org/TR/core-aam-1.1/",
-		"status": "WD",
-		"publisher": "W3C",
-		"authors": [
-			"Joseph Scheuhammer",
-			"Michael Cooper",
-			"Andi Snow-Weaver",
-			"Aaron Leventhal"
-		],
-		"etAl": true,
-		"deliveredBy": [
-			"http://www.w3.org/WAI/ARIA/"
-		]
+		"aliasOf": "CORE-AAM-1.1",
 	},
         "DPUB-ARIA": {
-                "title": "Digital Publishing WAI-ARIA Module 1.0",
-                "href": "http://www.w3.org/TR/dpub-aria-1.0/",
-                "status": "WD",
-                "publisher": "W3C",
-                "authors": [
-                        "Matt Garrish",
-                        "Tzviya Siegman",
-                        "Markus Gylling",
-                        "Shane McCarron"
-                ],
-                "etAl": true,
-                "deliveredBy": [
-                        "http://www.w3.org/WAI/ARIA/"
-                ]
+		"aliasOf": "DPUB-ARIA-1.0",
         },
         "GRAPHICS-ARIA": {
-                "title": "WAI-ARIA Graphics",
-                "href": "http://www.w3.org/TR/graphics-aria-1.0/",
-                "status": "WD",
-                "publisher": "W3C",
-                "authors": [
-                        "Amelia Bellamy-Royds",
-                        "Fred Esch",
-                        "Rich Schwerdtfeger",
-                        "Leonie Watson"
-                ],
-                "etAl": true,
-                "deliveredBy": [
-                        "http://www.w3.org/WAI/ARIA/",
-                        "http://www.w3.org/SVG/"
-                ]
+		"aliasOf": "GRAPHICS-ARIA-1.0",
         },
         "GRAPHICS-AAM": {
-                "title": "Graphics Accessibility API Mappings",
-                "href": "http://www.w3.org/TR/graphics-aam-1.0/",
-                "status": "WD",
-                "publisher": "W3C",
-                "authors": [
-                        "Amelia Bellamy-Royds",
-                        "Fred Esch",
-                        "Richard Schwerdtfeger"
-                ],
-                "etAl": true,
-                "deliveredBy": [
-                        "http://www.w3.org/WAI/ARIA/",
-                        "http://www.w3.org/SVG/"
-                ]
+		"aliasOf": "GRAPHICS-AAM-1.0",
         },
 	"EPUB-Content": {
 		"href": "http://www.idpf.org/epub/31/spec/epub-contentdocs.html",
@@ -106,62 +24,15 @@ var biblio = {
 		"publisher": "IDPF"
 	},
 	"HTML-AAM": {
-		"authors": [
-			"Steve Faulkner",
-			"Jason Kiss",
-			"Cynthia Shelly",
-			"Alexander Surkov"
-		],
-		"etAl": true,
-		"href": "http://www.w3.org/TR/html-aam-1.0/",
-		"title": "HTML Accessibility API Mappings 1.0",
-		"status": "WD",
-		"publisher": "W3C",
-		"deliveredBy": [
-			"http://www.w3.org/html",
-			"http://www.w3.org/WAI/PF/"
-		]
+		"aliasOf": "HTML-AAM-1.0",
 	},
 	"SVG-AAM": {
-		"title": "SVG2 Accessibility API Mappings 1.0",
-		"href": "http://www.w3.org/TR/svg-aam-1.0/",
-		"status": "WD",
-		"publisher": "W3C",
-		"authors": [
-			"Amelia Bellamy-Royds",
-			"Richard Schwerdtfeger"
-		],
-		"etAl": true,
-		"deliveredBy": [
-			"http://www.w3.org/WAI/PF/"
-		]
+		"aliasOf": "SVG-AAM-1.0",
 	},
 	"SVG1": {
-		"title": "Scalable Vector Graphics (SVG) 1.0 Specification",
-		"href": "http://www.w3.org/TR/SVG10/",
-		"status": "REC",
-		"publisher": "W3C",
-		"authors": [
-			"John Ferraiolo"
-		],
-		"etAl": true,
-		"deliveredBy": [
-		"http://www.w3.org/SVG"
-		]
+		"aliasOf": "SVG",
 	},
 	"WAI-ARIA": {
-		"title": "Accessible Rich Internet Applications (WAI-ARIA) 1.1",
-		"href": "http://www.w3.org/TR/wai-aria-1.1/",
-		"status": "WD",
-		"publisher": "W3C",
-		"authors": [
-			"James Craig",
-			"Michael Cooper",
-			"Shane McCarron"
-		],
-		"etAl": true,
-		"deliveredBy": [
-			"http://www.w3.org/WAI/PF/"
-		]
+		"aliasOf": "WAI-ARIA-1.1",
 	}
 };
