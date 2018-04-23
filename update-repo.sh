a0613048afc0868b80b5bc75b6b9762a91a3d812
Nick Schonning <Nick.Schonning@cic.gc.ca>
2018-04-23 14:07:35 -0400

    build: Add README.md to child repo copying

--- update-repo.sh
+++ update-repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone --depth=1 --branch=master https://github.com/w3c/$1.git ../../$1
-cp -r acknowledgements.html biblio.js terms.html css img script utility ../../$1/common
+cp -r README.md acknowledgements.html biblio.js terms.html css img script utility ../../$1/common
 cd ../../$1
 git add -A .
 git commit -m "TRAVIS-CI update from aria-common"
