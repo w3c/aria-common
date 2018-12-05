bc026e9ca45ca0f058ef95f5950cde9e252ec84a
Michael Cooper <cooper@w3.org>
2018-12-05 14:25:20 -0500

    check out correct branch

--- update-repo.sh
+++ update-repo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone --depth=1 --branch=master https://github.com/w3c/$1.git ../../$1
+git clone --depth=1 --branch=$2 https://github.com/w3c/$1.git ../../$1
 cp -r README.md acknowledgements.html biblio.js terms.html css img script utility ../../$1/common
 cd ../../$1
 git add -A .
