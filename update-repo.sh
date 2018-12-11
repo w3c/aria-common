2aef030a2f91fea938a08d001fcd6b4076e13b76
Michael Cooper <cooper@w3.org>
2018-12-11 11:35:35 -0500

    handle copy to multiple repos and branches better

--- update-repo.sh
+++ update-repo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-git clone --depth=1 --branch=$2 https://github.com/w3c/$1.git ../../$1
-cp -r README.md acknowledgements.html biblio.js terms.html css img script utility ../../$1/common
-cd ../../$1
+git clone --depth=1 --branch=$2 https://github.com/w3c/$1.git $1-$2
+cp -r README.md acknowledgements.html biblio.js terms.html css img script utility $1-$2/common
+cd $1-$2
 git add -A .
 git commit -m "TRAVIS-CI update from aria-common"
 git push "https://${GH_TOKEN}@github.com/w3c/$1.git" $2 > /dev/null 2>&1
-rm -rf ../../$1
+cd ..
