7a5bd122d2dc7d19a3baeeb74729d7f3810850a2
Michael Cooper <cooper@w3.org>
2019-01-24 10:00:08 -0500

    copy new acknowledgements subdir

--- update-repo.sh
+++ update-repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone --depth=1 --branch=$2 https://github.com/w3c/$1.git $1-$2
-cp -r README.md acknowledgements.html biblio.js terms.html css img script utility $1-$2/common
+cp -r README.md acknowledgements.html biblio.js terms.html acknowledgements css img script utility $1-$2/common
 cd $1-$2
 git add -A .
 git commit -m "TRAVIS-CI update from aria-common"
