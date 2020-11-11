ab97e72f8ebb8469d6233587a382274a888d7584
Nick Schonning <nschonni@gmail.com>
2020-11-11 16:46:17 -0500

    chore: Switch from Travis to GitHub Actions

--- update-repo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git clone --depth=1 --branch=$2 https://github.com/w3c/$1.git $1-$2
-[[ -d $1-$2/common ]] || mkdir -p $1-$2/common
-cp -r README.md acknowledgements.html biblio.js terms.html acknowledgements css img script utility $1-$2/common
-cd $1-$2
-git add -A .
-git commit -m "TRAVIS-CI update from aria-common"
-git push "https://${GH_TOKEN}@github.com/w3c/$1.git" $2 > /dev/null 2>&1
-cd ..
